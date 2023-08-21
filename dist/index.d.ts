da42b2128d667769e42e04c5655abe536a10d8b6
Bora B <borabarteks@outlook.com>
2023-08-21 13:49:47 +0300

    fix: missing function export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { ChromeStorageUtils } from "./chrome-storage-utils/index.js";
 export { injectComponent } from "./injection-utils/index.js";
+export { safeInjectComponent } from "./injection-utils/index.js";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ChromeStorageUtils } from "./chrome-storage-utils/index.js";
 
 export { injectComponent } from "./injection-utils/index.js";
+export { safeInjectComponent } from "./injection-utils/index.js";
